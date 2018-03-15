2601cb837fdbc28c8f7619932762b77b4f06036f
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-03-15 16:53:27 -0400

    test that there can be an associated user with an activity. add a this->signIn() helper for the test parent class

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -14,6 +14,8 @@ class ActivityTest extends TestCase
   {
     parent::setUp();
 
+    $this->signIn();
+
     $this->activity = create('App\Activity', [
       'type' => '\App\ActivityTypes\Driving',
       'emissions' => 4500000
@@ -27,8 +29,18 @@ class ActivityTest extends TestCase
   }
 
   /** @test */
-  function an_activity_has_an_emissions_level_in_kg()
+  function an_activity_has_an_emissions_level_in_grams()
   {
     $this->assertEquals(4500000,  $this->activity->emissions);
-  }  
+  }
+
+  /** @test */
+  function an_activity_can_belong_to_a_user()
+  {
+    $this->assertEquals(null, $this->activity->user_id);
+
+    $this->activity->update('user_id', auth()->id);
+
+    $this->assertEquals(auth()->id, $this->activity->fresh()->user->id);
+  }
 }
